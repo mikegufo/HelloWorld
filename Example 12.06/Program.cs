b0f32c288c66b4a348286dfc55f5d073ea2e7bd5
Vadim <nikegufo666@gmail.com>
2023-06-12 22:01:12 +0500

    Control Commit

---
+++ Example 12.06/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число :");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число :");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if ((numberB * numberB) == numberA)
+{
+Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Нет");
+}
+Console.ReadKey();
---
+++ Example DayWeek/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int DayWeek = Convert.ToInt32(Console.ReadLine());
+console.w`;
+switch (DayWeek)
+{
+case 1:
+Console.WriteLine("Понедельник");
+break;
+case 2:
+Console.WriteLine("Вторник");
+break;
+case 3:
+Console.WriteLine("Среда");
+break;
+case 4:
+Console.WriteLine("Четверг");
+break;
+case 5:
+Console.WriteLine("Пятница");
+break;
+case 6:
+Console.WriteLine("Суббота");
+break;
+case 7:
+Console.WriteLine("Воскресенье");
+break;
+default:
+Console.WriteLine("Ошибка");
+break;
+}
