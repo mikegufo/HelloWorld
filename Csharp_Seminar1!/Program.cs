51b4c9c462ac3996bbf09679561e13f6ff88a2ec
Vadim <nikegufo666@gmail.com>
2023-06-12 22:22:58 +0500

    Добавил задание под номером 1

--- Csharp_Seminar1!/Program.cs
+++ Csharp_Seminar1!/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(System.Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(System.Console.ReadLine());
+
+int max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+
+Console.Write("max = ");
+Console.WriteLine(max);
