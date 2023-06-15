13635abc12d30c164398fcd2290a30160bb352be
Vadim <nikegufo666@gmail.com>
2023-06-15 12:22:45 +0500

    Добавил третье задание

---
+++ Csharp_Seminar3!/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите первое число :");
+
+int numberA = Convert.ToInt32(System.Console.Read());
+
+if (numberA % 2 == 0)System.Console.WriteLine(true);
+
+else System.Console.WriteLine(false);
