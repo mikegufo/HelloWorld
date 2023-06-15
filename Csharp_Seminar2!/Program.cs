ebe2733eef6b50accc54763e5358968c236a2f4c
Vadim <nikegufo666@gmail.com>
2023-06-15 12:06:53 +0500

    Добавил задание номер 2

---
+++ Csharp_Seminar2!/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(System.Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(System.Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int numberC = Convert.ToInt32(System.Console.ReadLine());
+
+int max = numberA;
+
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
