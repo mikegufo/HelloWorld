22e87850dd37f5952bf7ccab201865f642ce2455
Vadim <nikegufo666@gmail.com>
2023-06-16 01:31:18 +0500

    Chisla Kraynie

---
+++ Examples Chisla Kraynie/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число :");
+int numberA = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+if (numberA > 99 && numberA < 1000)
+{
+numberA = numberA % 100;
+Console.WriteLine("Последняя цифра " + numberA);
+}
+else
+{
+Console.WriteLine("Введите корректное число от 1 до 999");
+}
